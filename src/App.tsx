07f18ec674414652f41c5e92ccd0de9b7a745a96
EdieAprovita <edieveg316@gmail.com>
2020-08-06 14:10:52 -0500

    Save task

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import React, { Fragment, useState } from 'react';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
 
 function App(): JSX.Element {
-  const [newTask, setNewTask] = useState('');
+  const [newTask, setNewTask] = useState<string>('');
+  const [task, setTask] = useState<ITask[]>([]);
 
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
-    console.log(newTask);
+    addTask(newTask);
+    setNewTask('');
+  };
+
+  const addTask = (name: string) => {
+    const newTask: ITask[] = [...task, { name, done: false }];
+    setTask(newTask);
   };
   return (
     <Fragment>
       <form onSubmit={handleSubmit}>
-        <input type='text' onChange={(e) => setNewTask(e.target.value)} />
+        <input
+          type='text'
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+        />
         <button>Save</button>
       </form>
+      {task.map((t: ITask, i: number) => {
+        return <h1 key={i}>{t.name}</h1>;
+      })}
     </Fragment>
   );
 }
