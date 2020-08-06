079fcf1084ae6064a9fb9909126314546d9ad028
EdieAprovita <edieveg316@gmail.com>
2020-08-06 13:56:47 -0500

    Create a task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+
+type FormElement = React.FormEvent<HTMLFormElement>;
 
 function App(): JSX.Element {
-  return <h1>Hello world</h1>;
+  const [newTask, setNewTask] = useState('');
+
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    console.log(newTask);
+  };
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={(e) => setNewTask(e.target.value)} />
+        <button>Save</button>
+      </form>
+    </Fragment>
+  );
 }
 
 export default App;
